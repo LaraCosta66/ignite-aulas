ea04447cbc470d70d443154acf481c5742990bb5
Lara Costa <larasmila64@gmail.com>
2023-05-16 15:16:46 -0300

    feat: change component format to typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
+
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+     
+    </>
+  );
+}
